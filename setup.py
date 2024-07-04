0c6b7570b475ad1f302c38ce985a36ca5cf53013
Zaid Hani <99472607+ZaidHani@users.noreply.github.com>
2024-07-04 13:58:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import sqlalchemy
 import kaggle
 
 # setting up the main cars dataset
-# kaggle.api.authenticate()
-# kaggle.api.dataset_download_files(dataset='ananaymital/us-used-cars-dataset', path='./data', unzip=True)
+kaggle.api.authenticate()
+kaggle.api.dataset_download_files(dataset='ananaymital/us-used-cars-dataset', path='./data', unzip=True)
 
 # setting up the dataset:
 
